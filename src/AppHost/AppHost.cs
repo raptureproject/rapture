07063ae8e4b4785a51cbf188f6c48505247163a7
Andrew Babbitt <andrew.babbitt@outlook.com>
2026-01-02 16:48:48 -0700

    chore: add apphost

---
+++ src/AppHost/AppHost.cs
@@ -0,0 +1,6 @@
+﻿// Licensed to the Rapture Project under one or more agreements.
+// The Rapture Project licenses this file to you under the MIT license.
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.Build().Run();
