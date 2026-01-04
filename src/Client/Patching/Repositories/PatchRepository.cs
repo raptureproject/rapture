1abd1a72d70cdd0ba5e0161e7525ac5e47ed6b95
Andrew Babbitt <andrew.babbitt@outlook.com>
2026-01-04 00:16:47 -0700

    fix: we still need the preinstalled patch versions

--- src/Client/Patching/Repositories/PatchRepository.cs
+++ src/Client/Patching/Repositories/PatchRepository.cs
@@ -17,13 +17,19 @@ public class PatchRepository(ClientEngine clientEngine)
     /// </summary>
     public ReadOnlyCollection<PatchInfo> PatchInfo => GetPatchInfo();
 
+    private readonly ReadOnlyCollection<PatchInfo> _preinstalledPatches =
+    [
+        new() { Platform = "win32", Channel = "release", Type = "boot", Version = "2010.07.10.0000", BuildTime = new(2010, 07, 10), RepositoryHash = "2d2a390f", FileSize = 0  },
+        new() { Platform = "win32", Channel = "release", Type = "game", Version = "2010.07.10.0000", BuildTime = new(2010, 07, 10), RepositoryHash = "48eca647", FileSize = 0  }
+    ];
+
     private ReadOnlyCollection<PatchInfo> GetPatchInfo()
     {
         var patchInfo = clientEngine.Torrents.Select(torrent =>
         {
             var patchFile = torrent.Files.First();
 
-            var type = patchFile.FullPath.StartsWith("2d2a390f") ? "boot" : "game";
+            var type = patchFile.Path.StartsWith("2d2a390f") ? "boot" : "game";
             var version = Path.GetFileNameWithoutExtension(patchFile.Path)[1..];
             var versionParts = version.Split('.');
             var buildTime = new DateOnly(int.Parse(versionParts[0]), int.Parse(versionParts[1]), int.Parse(versionParts[2]));
@@ -42,6 +48,9 @@ public class PatchRepository(ClientEngine clientEngine)
             };
         });
 
-        return new([.. patchInfo]);
+        return new([
+            .. _preinstalledPatches,
+            .. patchInfo
+        ]);
     }
 }
