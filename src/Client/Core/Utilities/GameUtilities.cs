2e1e907f3da738a8a4e7ad1ed01d1e0fcc93a5fc
Andrew Babbitt <andrew.babbitt@outlook.com>
2026-01-08 13:41:52 -0700

    chore: add game and hashing utilities

---
+++ src/Client/Core/Utilities/GameUtilities.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the Rapture Project under one or more agreements.
+// The Rapture Project licenses this file to you under the MIT license.
+
+using Microsoft.Win32;
+
+namespace Rapture.Client.Core.Utilities;
+
+/// <summary>
+/// Provides utility methods for retrieving game-related information from the system.
+/// </summary>
+public static class GameUtilities
+{
+    /// <summary>
+    /// Retrieves the full installation path of the game from the Windows registry.
+    /// </summary>
+    /// <returns>A string containing the full path to the game's installation directory if found; otherwise, null.</returns>
+    public static string? GetGameInstallPath()
+    {
+        var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{F2C4E6E0-EB78-4824-A212-6DF6AF0E8E82}");
+
+        if (key is null)
+        {
+            return null;
+        }
+
+        if (key.GetValue("InstallLocation") is not string installLocation ||
+            key.GetValue("DisplayName") is not string displayName)
+        {
+            return null;
+        }
+
+        return Path.Combine(installLocation, displayName);
+    }
+}
---
+++ src/Client/Core/Utilities/HashingUtilities.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the Rapture Project under one or more agreements.
+// The Rapture Project licenses this file to you under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
+namespace Rapture.Client.Core.Utilities;
+
+/// <summary>
+/// Provides utility methods for computing cryptographic hashes of files.
+/// </summary>
+public static class HashingUtilities
+{
+    /// <summary>
+    /// Computes the SHA-1 hash of the contents of the specified file and returns it as a hexadecimal string.
+    /// </summary>
+    /// <param name="path">The path to the file to hash. The file must exist and be accessible for reading.</param>
+    /// <returns>A hexadecimal string representation of the SHA-1 hash of the file's contents.</returns>
+    public static byte[] SHA1HashFile(string path)
+    {
+        using var file = new FileStream(path, FileMode.Open, FileAccess.Read);
+        return SHA1HashStream(file);
+    }
+
+    /// <summary>
+    /// Computes the SHA-1 hash value for the data contained in the specified stream.
+    /// </summary>
+    /// <param name="stream">The input stream containing the data to be hashed.</param>
+    /// <returns>A byte array containing the SHA-1 hash of the stream's data.</returns>
+    [SuppressMessage("Security", "CA5350:Do Not Use Weak Cryptographic Algorithms", Justification = "This is for file hashing.")]
+    public static byte[] SHA1HashStream(Stream stream)
+    {
+        using var sha1 = SHA1.Create();
+        var hash = sha1.ComputeHash(stream);
+
+        return hash;
+    }
+}
