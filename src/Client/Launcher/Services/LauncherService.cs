5bbe4a34088a83001db0aeda7f31cebbebc8d284
Andrew Babbitt <andrew.babbitt@outlook.com>
2026-01-08 13:44:12 -0700

    chore: cleanup launcher service

--- src/Client/Launcher/Services/LauncherService.cs
+++ src/Client/Launcher/Services/LauncherService.cs
@@ -1,11 +1,9 @@
 ﻿// Licensed to the Rapture Project under one or more agreements.
 // The Rapture Project licenses this file to you under the MIT license.
 
-using Microsoft.Win32;
 using Microsoft.Win32.SafeHandles;
+using Rapture.Client.Core.Utilities;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Security.Cryptography;
 using System.Text;
 using Windows.Win32;
 using Windows.Win32.Foundation;
@@ -23,7 +21,7 @@ public class LauncherService : BackgroundService
     /// <inheritdoc/>
     public override Task StartAsync(CancellationToken cancellationToken)
     {
-        var gamePath = GetGameInstallPath();
+        var gamePath = GameUtilities.GetGameInstallPath();
 
         if (gamePath is null)
         {
@@ -76,24 +74,6 @@ public class LauncherService : BackgroundService
         }
     }
 
-    private static string? GetGameInstallPath()
-    {
-        var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{F2C4E6E0-EB78-4824-A212-6DF6AF0E8E82}");
-
-        if (key is null)
-        {
-            return null;
-        }
-
-        if (key.GetValue("InstallLocation") is not string installLocation ||
-            key.GetValue("DisplayName") is not string displayName)
-        {
-            return null;
-        }
-
-        return Path.Combine(installLocation, displayName);
-    }
-
     private static unsafe void StartBoot(string bootDirectory)
     {
         var bootPath = Path.Combine(bootDirectory, "ffxivboot.exe");
@@ -106,7 +86,7 @@ public class LauncherService : BackgroundService
             Environment.Exit(1);
         }
 
-        var bootHash = HashFile(bootPath);
+        var bootHash = Convert.ToHexString(HashingUtilities.SHA1HashFile(bootPath));
 
         // Version 2010.07.10.0000
         if (bootHash == "999EFB09D7D94C9C8106A75688CF3BED7C1FBA84")
@@ -117,30 +97,27 @@ public class LauncherService : BackgroundService
         _ = PInvoke.ResumeThread(process.hThread);
     }
 
-    [SuppressMessage("Security", "CA5350:Do Not Use Weak Cryptographic Algorithms", Justification = "This is for file hashing.")]
-    private static string HashFile(string path)
-    {
-        using var file = new FileStream(path, FileMode.Open, FileAccess.Read);
-        using var sha1 = SHA1.Create();
-        var hash = sha1.ComputeHash(file);
-
-        return Convert.ToHexString(hash);
-    }
-
     private static void PatchBoot(uint processId)
     {
         var processHandle = PInvoke.OpenProcess_SafeHandle(PROCESS_ACCESS_RIGHTS.PROCESS_ALL_ACCESS, false, processId);
         uint baseOffset = 0x400000;
 
-        // ver01.ffxiv.com -> 127.0.0.1
-        ApplyPatch(processHandle, baseOffset + 0x8E62DC, Encoding.ASCII.GetBytes("127.0.0.1\0\0\0\0\0\0\0"), 16);
+        // SetProcessAffinityMask
+        ApplyPatch(processHandle, baseOffset + 0x49CC, [.. Enumerable.Repeat<byte>(0x90, 30)]);
+
+        // SQEX::CDev::Engine::Vfx::Qix::Thread::ThreadManager::CreateEffectThread
+        ApplyPatch(processHandle, baseOffset + 0x6937BF, [0xB5, 0x01]);
 
         // rsa_verify
-        ApplyPatch(processHandle, baseOffset + 0x5DF64, [0x01, 0x00, 0x00, 0x00], 4);
+        ApplyPatch(processHandle, baseOffset + 0x5DF64, [0x01, 0x00, 0x00, 0x00]);
+
+        // ver01.ffxiv.com -> 127.0.0.1
+        ApplyPatch(processHandle, baseOffset + 0x8E62DC, Encoding.ASCII.GetBytes("127.0.0.1\0\0\0\0\0\0\0"));
     }
 
-    private static unsafe void ApplyPatch(SafeFileHandle processHandle, nuint address, byte[] patchData, nuint patchSize)
+    private static unsafe void ApplyPatch(SafeFileHandle processHandle, nuint address, byte[] patchData)
     {
+        var patchSize = (uint)patchData.Length;
         var unprotectResult = PInvoke.VirtualProtectEx(processHandle, (void*)address, patchSize, PAGE_PROTECTION_FLAGS.PAGE_EXECUTE_READWRITE, out var oldProtect);
         var writeResult = PInvoke.WriteProcessMemory(processHandle, (void*)address, patchData);
         var reprotectResult = PInvoke.VirtualProtectEx(processHandle, (void*)address, patchSize, oldProtect, out _);
